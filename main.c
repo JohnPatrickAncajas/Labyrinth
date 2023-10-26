652113681bc0ef1916f8e27c4e93c20e34bcd453
JohnPatrickAncajas <ancajasjohnpatrickdelapena@gmail.com>
2023-10-26 22:57:13 +0800

    Version 0.2
    Expanded the main function.

--- main.c
+++ main.c
@@ -3,15 +3,26 @@
 int main() {
 	
 	int level, experience, health, power, defense, speed;
-	char name[16];
+	char name[32], choice;
 	
+	printf("What is your name, traveler? ");
+	scanf("%32s", name);
 	
-	printf("Welcome to this world!");
+	printf("\nWelcome to this world, %s!", name);
 	
-	printf("\n\nWhat is your name, traveler? ");
-	scanf("%15s", name);
+	printf("\n\nYou find yourself alone in the plains with a dagger tattered clothes and a knife. In the distance, you see a huge forest and multiple hills. Where do you go?");
 	
-	printf("Are you ready to start your adventure, %s?", name);
+	printf("\n\nP: stay in the plains\nF: travel to the forest\nH: climb the hills\n");
+	
+	scanf("%s", &choice);
+	
+	if (choice == 'P') {
+	    printf("You stayed at the plains");
+	} else if (choice == 'F') {
+	    printf("You went to the forest");
+	} else if (choice == 'H') {
+	    printf("You went to hills");
+	}
 	
 	return 0;
 }
