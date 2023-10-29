02fb0c00c57f0aa092e037240332aa3917109a0a
JohnPatrickAncajas <ancajasjohnpatrickdelapena@gmail.com>
2023-10-29 21:16:35 +0800

    Verion 0.3
    Added firstRoom function.
    Started on Intro.
    Adjusted main function.

--- main.c
+++ main.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+void firstRoom(char name[16], bool *doorUnlocked) {
+    char choice;
+
+    while (*doorUnlocked == false) {
+        
+        printf("\n\nWhat do you do, %s?\n\nA: Investigate the Radio.\nB: Look around the Room.\nC: Open the Door.\n\n", name);
+        scanf(" %c", &choice);
+        
+        if (choice == 'A') {
+            printf("\nThe Radio seems to not work anymore.");
+        } else if (choice == 'B') {
+            printf("\nYou found a note under the bed saying 'Shout 1770 to open the door!'");
+            *doorUnlocked = true;
+        } else if (choice == 'C') {
+            printf("\n When you touched the door, a suddden electrice burst from it shocked you!");
+        }
+    }
+}
 
 int main() {
-	
-	int level, experience, health, power, defense, speed;
-	char name[32], choice;
-	
-	printf("What is your name, traveler? ");
-	scanf("%32s", name);
-	
-	printf("\nWelcome to this world, %s!", name);
-	
-	printf("\n\nYou find yourself alone in the plains with a dagger tattered clothes and a knife. In the distance, you see a huge forest and multiple hills. Where do you go?");
-	
-	printf("\n\nP: stay in the plains\nF: travel to the forest\nH: climb the hills\n");
-	
-	scanf("%s", &choice);
-	
-	if (choice == 'P') {
-	    printf("You stayed at the plains");
-	} else if (choice == 'F') {
-	    printf("You went to the forest");
-	} else if (choice == 'H') {
-	    printf("You went to hills");
-	}
-	
-	return 0;
+    int  level = 1, experience = 0, health = 3,  power = 1, defense = 1, speed = 1;
+    char name[16], startChoice;
+    bool doorUnlocked = false;
+
+    // printf("Welcome to Game! This is a simple choice based text game. Type H to open the help menu, S to open your stats and Y to start the game!");
+    
+    // scanf("%c", &startChoice);
+    
+    // if (startChoice == "H") {
+    //     printf("H: Open the help menu\nS: Open the status menu\nY: Start the game\nX: Leave the game");
+    // } else if (startChoice == "S") {
+    //     printf("H: Open the help menu\nS: Open the status menu\nY: Start the game\nX: Leave the game");
+    // } else if (startChoice == "H") {
+    //     printf("H: Open the help menu\nS: Open the status menu\nY: Start the game\nX: Leave the game");
+    // } else {
+        
+    // }
+    
+    printf("\n\nWhat is your name? ");
+    scanf("%15s", name);
+
+    printf("\nYou slowly open your eyes, the world coming into focus. The room is unfamiliar, and the air is thick with an eerie silence. As you sit up, you realize you have no memory of how you got here or where 'here' even is. Panic starts to creep in.\n\nYou notice a door at the other end of the room. Odd noises from beyond the door make your heart race. With no other options, you muster your courage and decide to investigate the source of those sounds. It's time to find out what awaits you in this mysterious mansion.\n\nAs you stand up, a crackling voice comes from a nearby radio on a dusty table.\n\n'Welcome to the Maze, %s! You're the latest contestant in our thrilliiing game! Do you have what it takes to escape the mansion's treacherous depths?? Let the challenge begin!!'", name);
+
+    firstRoom(name, &doorUnlocked);
+
+    return 0;
 }
